0c27ab8cbf710e587249d8be62fb76e4b5f641d8
Антон Нурапкин <nurapkin99@mail.ru>
2023-10-04 00:11:15 +0300

    create func for f_vector

--- f_vector.py
+++ f_vector.py
@@ -1,17 +1,20 @@
 import numpy as np
+from helpers import search_nodes_in_domain, F_vector
+from params import b, n_x, n_y, THICKNESS
+from meshing import cells
+from plotly import graph_objects as go
 
 
-def create_f_vector(nodes_in_domain, B, b, weight, jacobian, global_indexes):
+def create_f_vector(F, b, nodes_in_domain, weight, jacobian, global_indexes):
     size = len(nodes_in_domain)
     f_local_vector_n_index = np.empty((0,2))
 
     for i in range(size):
-        B_i = B[:, i]
         f_local_vector_n_index = np.append(
             f_local_vector_n_index,
             [
                 [
-                    jacobian * weight * np.dot(np.transpose(B_i), b),
+                    THICKNESS * jacobian * weight * F[i] * b[2, 0],
                     global_indexes[i]
                 ]
             ],
@@ -26,3 +29,32 @@ def f_global_assemble(f_global, f_local_vector_n_indexes):
         f_global[int(elem[1])] += elem[0]
 
     return f_global
+
+
+def create_f_global(cells, n_x, n_y):
+
+    f_global = np.zeros((n_x * n_y, 1))
+
+    for i in range(len(cells)):
+        for j in range(len(cells[i])):
+            for point in cells[i][j].gauss_points:
+
+                nodes_in_domain, global_indexes = search_nodes_in_domain(q_point=point, current_cell=cells[i][j])
+
+                F = F_vector(q_point=point, nodes_in_domain=nodes_in_domain)
+
+                f_local_vector_n_indexes = create_f_vector(
+                    F,
+                    b,
+                    nodes_in_domain,
+                    point.weight,
+                    cells[i][j].jacobian,
+                    global_indexes
+                )
+
+                f_global = f_global_assemble(f_global, f_local_vector_n_indexes)
+
+    return f_global
+
+
+f = create_f_global(cells=cells, n_x=n_x, n_y=n_y)
