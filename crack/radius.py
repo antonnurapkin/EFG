de8dd68a00385a9e42779364e53e889b9e16f3a7
Антон Нурапкин <nurapkin99@mail.ru>
2024-05-13 23:45:22 +0300

    fix: add new derivative for radius

--- crack/radius.py
+++ crack/radius.py
@@ -12,14 +12,18 @@ def r_diffraction_method(point, crack_top, node):
 
 def drdx(point, crack_top, node):
     s0 = distance(x1=point.x, y1=point.y, x2=node.x, y2=node.y)
+    s1 = distance(x1=node.x, y1=node.y, x2=crack_top.x, y2=crack_top.y)
     s2 = distance(x1=point.x, y1=point.y, x2=crack_top.x, y2=crack_top.y)
 
-    return ((point.x - crack_top.x) * (s0 ** 2) - (point.x - node.x) * (s2 ** 2)) / ((s0 ** 3) * s2) / DS
+    return (LAMBDA * (((s1 + s2) / s0) ** (LAMBDA - 1)) * ((point.x - crack_top.x) / s2) +
+            (1 - LAMBDA) * (((s1 + s2) / s0) ** LAMBDA) * ((point.x - crack_top.x) / s0)) / DS
+
 
 
 def drdy(point, crack_top, node):
     s0 = distance(x1=point.x, y1=point.y, x2=node.x, y2=node.y)
+    s1 = distance(x1=node.x, y1=node.y, x2=crack_top.x, y2=crack_top.y)
     s2 = distance(x1=point.x, y1=point.y, x2=crack_top.x, y2=crack_top.y)
 
-    return ((point.y - crack_top.y) * (s0 ** 2) - (point.y - node.y) * (s2 ** 2)) / ((s0 ** 3) * s2) / DS
-
+    return (LAMBDA * (((s1 + s2) / s0) ** (LAMBDA - 1)) * ((point.y - crack_top.y) / s2) +
+            (1 - LAMBDA) * (((s1 + s2) / s0) ** LAMBDA) * ((point.y - crack_top.y) / s0)) / DS
